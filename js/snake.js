aa6677685ee25772a57de2c6beace6110ea93ea4
atironati <atironati@gmail.com>
2016-04-14 17:46:39 -0400

    Create a world ending death check and a non-world-ending death check

--- js/snake.js
+++ js/snake.js
@@ -47,7 +47,7 @@ $(function(){
       // set old head to regular body part
       this.grid[head_pos.x][head_pos.y][0].className = "snake-square";
 
-      if(this.deathCheck(new_head_pos)) return;
+      if(this.worldEndingDeathCheck(new_head_pos)) return;
 
       // draw the head in its new position
       this.grid[new_head_pos.x][new_head_pos.y][0].className = "snake-head-square";
@@ -66,19 +66,32 @@ $(function(){
       var new_head_pos_y = head_pos.y + this.direction[1];
       return new Point(new_head_pos_x,new_head_pos_y);
     },
+    nextHeadPositionWithDirection: function(dir) {
+      var head_pos = this.body[0];
+      var new_head_pos_x = head_pos.x + dir[0];
+      var new_head_pos_y = head_pos.y + dir[1];
+      return new Point(new_head_pos_x,new_head_pos_y);
+    },
+    worldEndingDeathCheck: function(new_head_pos) {
+      if (this.deathCheck(new_head_pos)) {
+        this.gameWorld.endGame();
+        return true;
+      } else {
+        return false;
+      }
+    },
     deathCheck: function(new_head_pos) {
       // is the snake about to go off the grid?
       if ((new_head_pos.x < 0 || new_head_pos.x >= this.grid.length) ||
           (new_head_pos.y < 0 || new_head_pos.y >= this.grid[0].length)) {
-        this.gameWorld.endGame();
         return true;
       }
 
       // is the snake about to eat itself?
       if (this.grid[new_head_pos.x][new_head_pos.y][0].className === "snake-square") {
-        this.gameWorld.endGame();
         return true;
       }
+
       return false;
     },
     foodCheck: function ( food ) {
